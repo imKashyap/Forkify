71c39a8177bf742b714df0df13e3c850821bae91
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-06-29 09:55:32 +0530

    Added feature to add recipes

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,74 +1,22 @@
-import { elements } from "./base";
+class SearchView {
+  _parentEl = document.querySelector('.search');
 
-export const getInput = () => elements.searchInput.value;
-export const clearInput = () => {
-    elements.searchInput.value = '';
-};
+  getQuery() {
+    const query = this._parentEl.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
 
-export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
-    elements.searchResPages.innerHTML = '';
-};
+  _clearInput() {
+    this._parentEl.querySelector('.search__field').value = '';
+  }
 
-export const highlightSelected = id => {
-    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
-    resultsArr.forEach(el => {
-        el.classList.remove('results__link--active');
+  addHandlerSearch(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
     });
-    document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
-};
-
-export const getRecipeTitle = (recipeTitle, lim = 17) => {
-    if (recipeTitle.length <= lim) return recipeTitle;
-    const newTitle = [];
-    recipeTitle.split(' ').reduce((acc, cur) => {
-        if (acc + cur.length <= lim) newTitle.push(cur);
-        return acc + cur.length;
-    }, 0);
-    return `${newTitle.join(' ')} ...`
+  }
 }
 
-const renderRecipe = recipe => {
-    const markup = `
-        <li>
-            <a class="results__link results__link--active" href="#${recipe.recipe_id}">
-                <figure class="results__fig">
-                    <img src="${recipe.image_url}" alt="${recipe.title}">
-                </figure>
-                <div class="results__data">
-                    <h4 class="results__name">${getRecipeTitle(recipe.title)}</h4>
-                    <p class="results__author">${recipe.publisher}</p>
-                </div>
-            </a>
-        </li>
-    `;
-    elements.searchResList.insertAdjacentHTML('beforeend', markup);
-};
-
-
-const createButton = (page, type) => `
-            <button class="btn-inline results__btn--${type}" data-goto=${page}>
-                    <svg class="search__icon">
-                        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
-                    </svg>
-                    <span>Page ${page}</span>
-            </button>`;
-
-const renderButtons = (page, numResults, resPerPage=10) => {
-    let button;
-    const pages = Math.ceil(numResults / resPerPage);
-    if (page == 1)
-        button = createButton(page + 1, 'next');
-    else if (page == pages) button = createButton(page - 1, 'prev');
-    else
-        button = createButton(page - 1, 'prev') +createButton(page + 1, 'next');
-    elements.searchResPages.insertAdjacentHTML('afterbegin', button);
-};
-
-
-export const renderResults = (recipes, resPerPage = 10, page = 1) => {
-    const start = (page - 1) * resPerPage;
-    const end = page * resPerPage;
-    recipes.slice(start, end).forEach(renderRecipe);
-    renderButtons(page, recipes.length, resPerPage);
-};
+export default new SearchView();
